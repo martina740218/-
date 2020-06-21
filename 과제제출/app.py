90e2bd6058e6c85602b038b5ba466861710fedac
martina740218 <65523238+martina740218@users.noreply.github.com>
2020-06-21 21:46:05 +0900

    4주차 과제 제출
    코딩은 여러번 확인했는데요. 에러 메세지의 원인을 찾지 못했으며
    몽고DB 실행후, 로보3T에 접속해서 봐도 DB 폴더가 생기지 않습니다.
    이에 완벽한 과제 제출은 안되었는데요. .... 어디가 잘못되어 안되는지 도움 부탁드립ㄴ디ㅏ.

---
+++ 과제제출/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+
+## API 역할을 하는 부분
+
+@app.route('/orders', methods=['POST'])
+def saving():
+	# 1. 클라이언트가 준 name, count가져오기.
+    name_receive = request.form['name_give']
+    count_receive = request.form['count_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+
+    # DB에 삽입할 정보 만들기 # 몽고 db에 삽입할 정보 만들기
+    order = {
+       'name': name_receive,
+       'count': count_receive,
+       'address': address_receive,
+       'phone': phone_receive
+    }
+
+    db.orders.insert_one(order)
+    
+	# 3. 성공 여부 & 성공 메시지 반환하기
+    return jsonify({'result': 'success', 'msg': '꽃으로 행복을 주문했습니다'})
+  
+
+@app.route('/orders', methods=['GET'])
+def listing():
+    result = list(db.orders.find({},{'_id':0}))
+    return jsonify({'result':'success', 'orders':orders})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
+
